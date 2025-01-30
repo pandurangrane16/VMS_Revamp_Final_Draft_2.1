518a97a7ce27904ab5b91c615a05361a04ee04ce
Pandurang Rane <pandurangrane16@gmail.com>
2025-01-30 10:06:21 +0530

    Publish Ops

--- src/environment.ts
+++ src/environment.ts
@@ -7,9 +7,9 @@ export const environment = {
     EncryptIV : "770A8A65DA156D24EE2A093277530142",
 
     //UAT Server Settings
-    SSE_Url : "https://172.19.32.51:8025/publish_api/notify",
-    PreviewPath : "https://172.19.32.51:8025/mediaLists/playlists/",
-    Socket_URL : "http://172.19.32.51:8030",
+    SSE_Url : "http://10.20.2.15:8026/publish_api/notify",
+    PreviewPath : "http://10.20.2.15:8026/mediaLists/playlists/",
+    Socket_URL : "http://10.20.2.15:8030",
     //Vadodara UAT Server Settings
     // SSE_Url : "http://10.100.43.157:8026/publish_api/notify",
     // Socket_URL : "10.20.1.111:8028",
